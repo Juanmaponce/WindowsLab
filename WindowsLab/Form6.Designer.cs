b7b128fa15c107062375162952cb7fc6c8d40a54
Juanmaponce <65909673+Juanmaponce@users.noreply.github.com>
2022-10-21 09:16:10 -0300

    Caso 6 completo

---
+++ WindowsLab/Form6.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsLab
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnNombreCompleto = new System.Windows.Forms.Button();
+            this.btnCompleto = new System.Windows.Forms.Button();
+            this.lblFechaNacimiento = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(121, 34);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(238, 31);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(124, 77);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(238, 77);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // btnNombreCompleto
+            // 
+            this.btnNombreCompleto.Location = new System.Drawing.Point(89, 186);
+            this.btnNombreCompleto.Name = "btnNombreCompleto";
+            this.btnNombreCompleto.Size = new System.Drawing.Size(122, 23);
+            this.btnNombreCompleto.TabIndex = 6;
+            this.btnNombreCompleto.Text = "Nombre y apellido";
+            this.btnNombreCompleto.UseVisualStyleBackColor = true;
+            this.btnNombreCompleto.Click += new System.EventHandler(this.btnNombreCompleto_Click);
+            // 
+            // btnCompleto
+            // 
+            this.btnCompleto.Location = new System.Drawing.Point(278, 185);
+            this.btnCompleto.Name = "btnCompleto";
+            this.btnCompleto.Size = new System.Drawing.Size(184, 23);
+            this.btnCompleto.TabIndex = 7;
+            this.btnCompleto.Text = "Nombre Apellido y edad";
+            this.btnCompleto.UseVisualStyleBackColor = true;
+            this.btnCompleto.Click += new System.EventHandler(this.btnCompleto_Click);
+            // 
+            // lblFechaNacimiento
+            // 
+            this.lblFechaNacimiento.AutoSize = true;
+            this.lblFechaNacimiento.Location = new System.Drawing.Point(124, 120);
+            this.lblFechaNacimiento.Name = "lblFechaNacimiento";
+            this.lblFechaNacimiento.Size = new System.Drawing.Size(108, 13);
+            this.lblFechaNacimiento.TabIndex = 5;
+            this.lblFechaNacimiento.Text = "Fecha de Nacimiento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(239, 120);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnCompleto);
+            this.Controls.Add(this.btnNombreCompleto);
+            this.Controls.Add(this.lblFechaNacimiento);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form6";
+            this.Text = "Form6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnNombreCompleto;
+        private System.Windows.Forms.Button btnCompleto;
+        private System.Windows.Forms.Label lblFechaNacimiento;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ WindowsLab/Form6.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLab
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNombreCompleto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+
+            string nombreCompleto = Concatenar(nombre, apellido);
+            MessageBox.Show(nombreCompleto);
+        }
+        private void btnCompleto_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int fechaNacimieto = dateTimePicker1.Value.Year;
+            string nombreCompleto = Concatenar(nombre, apellido, fechaNacimieto);
+            MessageBox.Show(nombreCompleto);
+        }
+
+        #region Metodos
+
+        public string Concatenar(string name, string lastname)
+        {
+            return name + ", " + lastname;
+        }
+
+        public string Concatenar(string name, string lastname, int birthday)
+        {
+            int fechaActual = DateTime.Today.Year;
+            int edad = fechaActual - birthday;
+            return "name" +", "+ lastname +" tiene "+ edad +" años";
+        }
+
+        #endregion
+
+    }
+}
